85fa8b04740a735f376cfe8a7cdd90f0a549c23e
Raghav-28 <kraghavgupta@gmail.com>
2025-04-03 21:49:52 +0530

    Arrays & Pattern Question

---
+++ MaxValueofOrderedTriplet.cpp
@@ -0,0 +1,17 @@
+//LEETCODE PROBLEM 2874 & 2873
+class Solution {
+    public:
+        long long maximumTripletValue(vector<int>& nums) {
+            int n = nums.size();
+            long long maxDiff =0;
+            long long maxi =0;
+            long long result =0;
+    
+            for(int i=0;i<n;i++){
+                result = max(result , (maxDiff *(long long)nums[i]));
+                maxDiff = max(maxDiff , maxi-nums[i]);
+                maxi= max(maxi, (long long)nums[i]);
+            }
+            return result;
+        }
+    };
