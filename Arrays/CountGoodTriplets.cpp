3b3e18e1695b7ecd8163af099240d6c5d257338e
Raghav-28 <kraghavgupta@gmail.com>
2025-04-15 18:30:38 +0530

    Solve easy one

---
+++ Arrays/CountGoodTriplets.cpp
@@ -0,0 +1,21 @@
+// LEETCODE 1534
+
+class Solution {
+    public:
+        int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+            int n = arr.size();
+            int count =0;
+            for(int i=0;i<=n-3;i++){
+                for(int j=i+1; j<=n-2;j++){
+                    if(abs(arr[i]-arr[j]) <=a){
+                        for(int k=j+1; k<= n-1; k++){
+                            if(abs(arr[j]-arr[k]) <= b && abs(arr[k]- arr[i])<= c){
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    };
