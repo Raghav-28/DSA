2b5ebc1d49e62ed64d72e6b9587eace053c2a93d
Raghav-28 <kraghavgupta@gmail.com>
2025-04-21 22:23:03 +0530

    1POTD & 1puzzle

---
+++ Hashmap/RabbitInForest.cpp
@@ -0,0 +1,20 @@
+// LEETCODE PROBLEM 
+
+class Solution {
+    public:
+        int numRabbits(vector<int>& answers) {
+              unordered_map<int, int> count;
+        for (int ans : answers) {
+            count[ans]++;
+        }
+    
+        int result = 0;
+        for (auto& [k, v] : count) {
+            int groupSize = k + 1;
+            int numGroups = ceil((double)v / groupSize);
+            result += numGroups * groupSize;
+        }
+    
+        return result;
+        }
+    };
